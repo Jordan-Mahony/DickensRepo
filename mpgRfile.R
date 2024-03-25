d34b3fd323abbe43ea79b2e90f86b81b6832637e
Jordan-Mahony <157429390+Jordan-Mahony@users.noreply.github.com>
2024-03-25 18:23:49 -0400

    Update mpgRfile.R(jordan)

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,10 @@
-# J Dickens  James Dickens
+# Jordan Mahony
+# The data source is the mpg data table
 
+library(ggplot2)
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+ggplot(data, aes(x = "",y = displ, fill = "Boxplot")) + geom_boxplot(color = "black", fill = "blue") + 
+labs(title = "Horizontal Boxplot for displ", x = NULL, y = "displ") + theme_minimal()
